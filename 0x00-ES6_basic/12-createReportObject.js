ede9c14bded37c787f3dc686b0aed37010dbb306
Horlew-myde <mezamune@gmail.com>
2023-03-20 18:15:44 -0400

    numero 12 hotfix

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,7 +1,8 @@
-import {createEmployeesObject} from './11-createEmployeesObject'
-
-
 export default function createReportObject(employeesList) {
-
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments() {
+      return Object.keys(employeesList).length;
+    },
+  };
 }
-
