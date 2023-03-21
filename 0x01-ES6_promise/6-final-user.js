cb7d3918e9bb6aca0688894718e168c8d044c857
Horlew-myde <mezamune@gmail.com>
2023-03-21 18:20:57 -0400

    numero 6 hotfix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,25 +1,9 @@
+// Handle multiple promises
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName).catch((error) => {
-      return error;
-    }),
-  ]).then((values) => {
-    let status;
-
-    const responses = [];
-    values.forEach((value) => {
-      if (value instanceof Error) status = 'failed';
-      else status = 'fulfilled';
-      responses.push({
-        status,
-        value,
-      });
-    });
-
-    return responses;
-  });
+function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
+  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
 }
+
+export default handleProfileSignup;
